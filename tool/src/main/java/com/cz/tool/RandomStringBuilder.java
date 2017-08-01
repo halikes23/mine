fe98835bc1fe3c2946a3b1e16e903f2c6b4efad6
Zhen Cheng <chengzhen000000@163.com>
2017-08-01 18:34:11 +0800

    add tool RandomStringBuilder.java

--- tool/src/main/java/com/cz/tool/RandomStringBuilder.java
+++ tool/src/main/java/com/cz/tool/RandomStringBuilder.java
@@ -1,7 +1,44 @@
 package com.cz.tool;
 
+import java.util.Random;
+
 public class RandomStringBuilder {
 	
+	private static final Random random = new Random();
 	
+	public static void main(String[] args) {
+		int length ;
+		if(args.length == 1){
+			length = Integer.parseInt(args[0]) ;
+		}else{
+			length = 32 ;
+		}
+		
+		String randomString = random(length);
+		
+		System.out.println(randomString);
+		
+	}
+	
+	public static String random(Integer length){
+		
+		StringBuilder rst = new StringBuilder();
+		
+		String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_+*/;:~!@#$%^&*()" ;
+		
+		String[] codes = str.split("") ;
+		
+		Integer codesLength = codes.length ;
+		
+		
+		
+		for(int i = 0 ; i < length ; i ++){
+			int index = random.nextInt(codesLength) ;
+			rst.append(codes[index]) ;
+		}
+		
+		return rst.toString() ;
+		
+	}
 	
 }
